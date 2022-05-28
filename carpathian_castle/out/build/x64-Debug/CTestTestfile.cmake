ecefa96aeb62708a136d60f91be0550e2af4a43c
Solene <suchuku35@gmail.com>
2022-05-28 23:15:27 +0200

    First cmake and generation

---
+++ carpathian_castle/out/build/x64-Debug/CTestTestfile.cmake
@@ -0,0 +1,14 @@
+# CMake generated Testfile for 
+# Source directory: C:/CPP_1/carpathian_castle/carpathian_castle
+# Build directory: C:/CPP_1/carpathian_castle/carpathian_castle/out/build/x64-Debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(that-test-I-made "C:/CPP_1/carpathian_castle/carpathian_castle/out/build/x64-Debug/unitTests.exe")
+set_tests_properties(that-test-I-made PROPERTIES  _BACKTRACE_TRIPLES "C:/CPP_1/carpathian_castle/carpathian_castle/CMakeLists.txt;125;add_test;C:/CPP_1/carpathian_castle/carpathian_castle/CMakeLists.txt;0;")
+subdirs("_deps/pugixml-build")
+subdirs("_deps/sfml-build")
+subdirs("tmxlite-build/tmxlite")
+subdirs("mainLauncher")
+subdirs("src")
+subdirs("googletest-build")
---
+++ carpathian_castle/out/build/x64-Debug/googletest-build/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/CPP_1/carpathian_castle/carpathian_castle/out/build/x64-Debug/googletest-src/googlemock
+# Build directory: C:/CPP_1/carpathian_castle/carpathian_castle/out/build/x64-Debug/googletest-build/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
